833578d79e2412d4e4f2166331a40a5b7f605342
EmilienMottet <emilien.mottet@grenoble-inp.org>
2022-10-04 17:05:17 +0200

    [step 1] return random movie

--- lib/bot_message_handler.ex
+++ lib/bot_message_handler.ex
@@ -1,4 +1,9 @@
 defmodule Volcamp.Bot.MessageHandler do
+  def handle_message("/movie", chat_id) do
+    [movie] = Movie.Discover.get_movies()
+    Volcamp.Bot.send_text("Si tu dois voir un film aujourd'hui\n#{movie.title}", chat_id)
+  end
+
   def handle_message(_message, chat_id) do
     Volcamp.Bot.send_text("\\_(°-°)_/", chat_id)
   end
