6eddf5a148b996cfc4060b1bac24e67b36631ea9
EmilienMottet <emilien.mottet@grenoble-inp.org>
2022-10-04 23:25:00 +0200

    [step 8] use Task.async_stream

--- lib/bot_message_handler.ex
+++ lib/bot_message_handler.ex
@@ -5,11 +5,11 @@ defmodule Volcamp.Bot.MessageHandler do
 
   def handle_message("/movie" <> genre, chat_id) do
     Movie.Discover.get_movies(genre: genre, size: 3)
-    |> Stream.map(fn movie ->
+    |> Task.async_stream(fn movie ->
       cover = Movie.Cover.get_cover(movie)
       {cover, movie}
     end)
-    |> Enum.map(fn {cover, movie} ->
+    |> Enum.map(fn {:ok, {cover, movie} } ->
       Volcamp.Bot.send_media(cover, movie.title, chat_id)
     end)
   end
